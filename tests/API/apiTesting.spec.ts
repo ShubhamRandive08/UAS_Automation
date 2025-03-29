a8bb711a5e607b9188eb51cf415b52bc1fe3dc28
Shubham Balavant Randive <srandive245@gmail.com>
2025-03-29 10:44:24 +0530

    api testing

--- tests/API/apiTesting.spec.ts
+++ tests/API/apiTesting.spec.ts
@@ -26,4 +26,19 @@ test.describe("API testing", async () => {
     const res1 = await reqContext.get("/studData");
     console.log(await res1.json());
   });
+  
+
+  test("API request GET practice 3", async () => {
+    const res = await reqContext2.get("http://localhost:3000/studData"); // This is the 3rd method to call the api wiht the store the base URL into the beforeAll hooks
+    console.log(await res.json());
+  });
+
+  test("API request GET practice 4", async ({ request }) => {
+    const res = await request.get(`${process.env.API_BASE_URL}/studData`,{
+      headers : {
+      'Content-Type' : 'application/json' // This is optional
+      }
+    }); // This is the 4th method to call the api with store the base URL into the .env file
+    console.log(await res.json());
+  });
 });
